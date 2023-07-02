beb26595c12f4d675b215dc1f64db5f3c6ea99b9
marti <martimanjospb@gmail.com>
2023-07-02 21:04:38 +0100

    teste

--- menu.py
+++ menu.py
@@ -1,21 +1,6 @@
 from tkinter import *
 from tkinter import ttk
 
-def show_table():
-    table_frame = Frame(root)
-    table_frame.pack(side='left', fill='both', expand=True)
-
-    # Cria o widget Treeview
-    treeview = ttk.Treeview(table_frame, columns=('Coluna1', 'Coluna2'), show='headings')
-    treeview.pack(side='left', fill='both', expand=True)
-
-    # Define o cabeçalho das colunas
-    treeview.heading('Coluna1', text='Menino')
-    treeview.heading('Coluna2', text='Menina')
-
-    # Insere os valores nas colunas
-    treeview.insert('', 'end', values=('Carlos', 'Joana'))
-
 def open_users():
     import main_users
     main_users.show_users(root)  # Chame a função do módulo main_users.py passando a janela root como argumento
