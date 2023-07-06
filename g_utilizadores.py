7df75c96045c4bdf70b0763e67e5d1642aaae5a9
marti <martimanjospb@gmail.com>
2023-07-06 19:31:18 +0100

    teste

---
+++ g_utilizadores.py
@@ -0,0 +1,358 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+import mysql.connector
+from tkinter import messagebox
+import re
+from tkcalendar import DateEntry
+
+# Estabelecer uma conexão com o banco de dados MySQL
+conexao = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="",
+    database="tf_prog_av"
+)
+
+# Buscar registros da tabela "q_utilizadores"
+def buscar_registros():
+    # Criar um cursor para executar comandos SQL
+    cursor = conexao.cursor()
+
+    # Execute a consulta para buscar os registros
+    cursor.execute("SELECT * FROM q_utilizadores")
+
+    # Obtenha os registros retornados pela consulta
+    registros = cursor.fetchall()
+
+    # Fechar o cursor
+    cursor.close()
+
+    # Limpar a tabela existente
+    for item in table.get_children():
+        table.delete(item)
+
+    # Adicionar os registros na tabela
+    for registro in registros:
+        table.insert("", "end", values=registro)
+
+# Apagar o utilizador selecionado na tabela
+def apagar_utilizador():
+    # Obter o ID do utilizador selecionado na tabela
+    selecionado = table.focus()
+    if selecionado:
+        utilizador_id = table.item(selecionado)["values"][0]
+
+        # Check if the utilizador is associated with a curso
+        cursor = conexao.cursor()
+        cursor.execute("SELECT aluno_id FROM q_alunos_cursos WHERE aluno_id = %s", (utilizador_id,))
+        result = cursor.fetchone()
+        cursor.close()  # Close the cursor object
+
+        if result:
+            messagebox.showinfo("Aviso", "O utilizador está associado a um curso e não pode ser excluído diretamente.")
+            return
+
+        # Confirmar a exclusão do utilizador
+        if messagebox.askyesno("Confirmação", f"Deseja apagar o utilizador com ID {utilizador_id}?"):
+            try:
+                # Executar o comando para apagar o utilizador
+                cursor = conexao.cursor()
+                cursor.execute("DELETE FROM q_utilizadores WHERE utilizador_id = %s", (utilizador_id,))
+
+                # Efetuar o commit das alterações
+                conexao.commit()
+
+                # Fechar o cursor
+                cursor.close()
+
+                # Atualizar a exibição dos registros
+                buscar_registros()
+
+            except mysql.connector.Error as error:
+                # Em caso de erro, imprimir a mensagem
+                print("Erro ao apagar o utilizador:", error)
+        else:
+            messagebox.showinfo("Aviso", "Nenhum utilizador selecionado.")
+
+# Criar um novo utilizador
+def criar_utilizador():
+    def salvar_utilizador():
+        # Obter os valores inseridos nos campos
+        nome = entry_nome.get()
+        email = entry_email.get()
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            # Mostrar erro de entrada
+            messagebox.showwarning("Aviso", "Invalid email address.")
+            return
+        contacto = entry_contacto.get()
+        if not contacto.isdigit() or len(contacto) != 9:
+            # Mostrar erro de entrada
+            messagebox.showwarning(text="Invalid phone number.")
+            return
+        morada = entry_morada.get()
+        localidade = entry_localidade.get()
+        nascimento = data_nascimento.get()
+        senha = entry_senha.get()
+        perfil = entry_perfil.get()
+
+        if perfil == "1":
+            messagebox.showinfo("Perfil", "O utilizador irá ficar registado como Aluno")
+        elif perfil == "2":
+            messagebox.showinfo("Perfil", "O utilizador irá ficar registado como Professor")
+        elif perfil == "3":
+            messagebox.showinfo("Perfil", "O utilizador irá ficar registado como Administrador")
+        else:
+            messagebox.showerror("Erro", "Não existe nenhum utilizador com esse ID")
+
+        # Criar um cursor para executar comandos SQL
+        cursor = conexao.cursor()
+
+        # Executar o comando para criar um novo utilizador
+        cursor.execute(
+            "INSERT INTO q_utilizadores (utilizador_nome, utilizador_email, utilizador_contacto, utilizador_morada, utilizador_localidade, utilizador_nascimento, utilizador_senha, utilizador_perfil) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+            (nome, email, contacto, morada, localidade, nascimento, senha, perfil))
+
+        # Efetuar o commit das alterações
+        conexao.commit()
+
+        # Fechar o cursor
+        cursor.close()
+
+        # Atualizar a exibição dos registros
+        buscar_registros()
+
+        # Fechar a janela de criação de utilizador
+        janela_utilizador.destroy()
+
+    # Criar a janela para criação de utilizador
+    janela_utilizador = tk.Toplevel()
+    janela_utilizador.title("Criar Utilizador")
+
+    label_nome = ttk.Label(janela_utilizador, text="Nome:")
+    entry_nome = ttk.Entry(janela_utilizador)
+
+    label_email = ttk.Label(janela_utilizador, text="Email:")
+    entry_email = ttk.Entry(janela_utilizador)
+
+    label_contacto = ttk.Label(janela_utilizador, text="Contacto:")
+    entry_contacto = ttk.Entry(janela_utilizador)
+
+    label_morada = ttk.Label(janela_utilizador, text="Morada:")
+    entry_morada = ttk.Entry(janela_utilizador)
+
+    label_localidade = ttk.Label(janela_utilizador, text="Localidade:")
+    entry_localidade = ttk.Entry(janela_utilizador)
+
+    label_nascimento = ttk.Label(janela_utilizador, text="Nascimento:")
+    data_nascimento = DateEntry(janela_utilizador, width=10, background='darkblue', foreground='white', borderwidth=2,
+                                   justify='center', date_pattern='yyyy-mm-dd')
+
+    label_senha = ttk.Label(janela_utilizador, text="Senha:")
+    entry_senha = ttk.Entry(janela_utilizador)
+
+    label_perfil = ttk.Label(janela_utilizador, text="Perfil:")
+    entry_perfil = ttk.Entry(janela_utilizador)
+
+    # Botão responsável por salvar o novo registo
+    btn_salvar = ttk.Button(janela_utilizador, text="Salvar", command=salvar_utilizador)
+
+    label_nome.pack(padx=5, pady=5)
+    entry_nome.pack(padx=5, pady=5)
+
+    label_email.pack(padx=5, pady=5)
+    entry_email.pack(padx=5, pady=5)
+
+    label_contacto.pack(padx=5, pady=5)
+    entry_contacto.pack(padx=5, pady=5)
+
+    label_morada.pack(padx=5, pady=5)
+    entry_morada.pack(padx=5, pady=5)
+
+    label_localidade.pack(padx=5, pady=5)
+    entry_localidade.pack(padx=5, pady=5)
+
+    label_nascimento.pack(padx=5, pady=5)
+    data_nascimento.pack(padx=5, pady=5)
+
+    label_senha.pack(padx=5, pady=5)
+    entry_senha.pack(padx=5, pady=5)
+
+    label_perfil.pack(padx=5, pady=5)
+    entry_perfil.pack(padx=5, pady=5)
+
+    btn_salvar.pack(padx=5, pady=10)
+
+# Alterar o utilizador selecionado na tabela
+def alterar_utilizador():
+    # Obter o utilizador selecionado na tabela
+    selecionado = table.focus()
+    if selecionado:
+        # Obter o ID do utilizador selecionado
+        utilizador_id = table.item(selecionado)['values'][0]
+
+        # Obter os dados do utilizador do banco de dados
+        cursor = conexao.cursor()
+        cursor.execute("SELECT * FROM q_utilizadores WHERE utilizador_id = %s", (utilizador_id,))
+        utilizador = cursor.fetchone()
+        cursor.close()
+
+        def atualizar_utilizador():
+            # Obter os valores inseridos nos campos
+            nome = entry_nome.get()
+            email = entry_email.get()
+            if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+                # Mostrar erro de entrada
+                messagebox.showwarning("Aviso", "Invalid email address.")
+                return
+            contacto = entry_contacto.get()
+            if not contacto.isdigit() or len(contacto) != 9:
+                # Mostrar erro de entrada
+                messagebox.showwarning('Warning', "Invalid phone number.")
+                return
+            morada = entry_morada.get()
+            localidade = entry_localidade.get()
+            nascimento = data_nascimento.get()
+            senha = entry_senha.get()
+            perfil = entry_perfil.get()
+
+            if perfil == "1":
+                messagebox.showinfo("Perfil", "O utilizador irá ficar registado como Aluno")
+            elif perfil == "2":
+                messagebox.showinfo("Perfil", "O utilizador irá ficar registado como Professor")
+            elif perfil == "3":
+                messagebox.showinfo("Perfil", "O utilizador irá ficar registado como Administrador")
+            else:
+                messagebox.showerror("Erro", "Não existe nenhum utilizador com esse ID")
+
+            # Criar um cursor para executar comandos SQL
+            cursor = conexao.cursor()
+
+            # Executar o comando para atualizar o utilizador
+            cursor.execute(
+                "UPDATE q_utilizadores SET utilizador_nome = %s, utilizador_email = %s, utilizador_contacto = %s, utilizador_morada = %s, utilizador_localidade = %s, utilizador_nascimento = %s, utilizador_senha = %s, utilizador_perfil = %s WHERE utilizador_id = %s",
+                (nome, email, contacto, morada, localidade, nascimento, senha, perfil, utilizador_id))
+
+            # Efetuar o commit das alterações
+            conexao.commit()
+
+            # Fechar o cursor
+            cursor.close()
+
+            # Fechar a janela de alteração de utilizador
+            janela_alterar.destroy()
+
+            # Atualizar a exibição dos registros
+            buscar_registros()
+
+        # Criar uma nova janela para editar os dados do utilizador
+        janela_alterar = tk.Toplevel()
+        janela_alterar.title("Alterar Utilizador")
+
+        # Preencher os campos de entrada com os dados atuais do utilizador
+        tk.Label(janela_alterar, text="Nome:").grid(row=0, column=0)
+        entry_nome = tk.Entry(janela_alterar)
+        entry_nome.insert(0, utilizador[1])
+        entry_nome.grid(row=0, column=1)
+
+        tk.Label(janela_alterar, text="Email:").grid(row=1, column=0)
+        entry_email = tk.Entry(janela_alterar)
+        entry_email.insert(0, utilizador[2])
+        entry_email.grid(row=1, column=1)
+
+        tk.Label(janela_alterar, text="Contacto:").grid(row=2, column=0)
+        entry_contacto = tk.Entry(janela_alterar)
+        entry_contacto.insert(0, utilizador[3])
+        entry_contacto.grid(row=2, column=1)
+
+        tk.Label(janela_alterar, text="Morada:").grid(row=3, column=0)
+        entry_morada = tk.Entry(janela_alterar)
+        entry_morada.insert(0, utilizador[4])
+        entry_morada.grid(row=3, column=1)
+
+        tk.Label(janela_alterar, text="Localidade:").grid(row=4, column=0)
+        entry_localidade = tk.Entry(janela_alterar)
+        entry_localidade.insert(0, utilizador[5])
+        entry_localidade.grid(row=4, column=1)
+
+        tk.Label(janela_alterar, text="Nascimento:").grid(row=5, column=0)
+        data_nascimento = DateEntry(janela_alterar, width=12, background='darkblue', foreground='white',
+                                        borderwidth=2,
+                                        justify='center', date_pattern='yyyy-mm-dd')
+
+        data_nascimento.insert(0, utilizador[6])
+        data_nascimento.grid(row=5, column=1)
+
+        tk.Label(janela_alterar, text="Senha:").grid(row=6, column=0)
+        entry_senha = tk.Entry(janela_alterar)
+        entry_senha.insert(0, utilizador[7])
+        entry_senha.grid(row=6, column=1)
+
+        tk.Label(janela_alterar, text="Perfil:").grid(row=7, column=0)
+        entry_perfil = tk.Entry(janela_alterar)
+        entry_perfil.insert(0, utilizador[8])
+        entry_perfil.grid(row=7, column=1)
+
+        # Botão para atualizar o utilizador
+        btn_atualizar = tk.Button(janela_alterar, text="Atualizar", command=atualizar_utilizador)
+        btn_atualizar.grid(row=8, column=0, columnspan=2)
+        # Caso o usuário se esqueça de selecionar um registo na tabela
+    else:
+        messagebox.showwarning("Aviso", "Selecione um utilizador para atualizar.")
+
+# Criar uma interface gráfica para exibir registros da tabela no formato em árvore (treeview)
+def criar_interface(content_frame):
+    global table, frame# Variações globais
+
+    # Remove todos os widgets filhos existentes
+    for widget in content_frame.winfo_children():
+        widget.destroy()
+
+    # Cria um novo frame dentro do content_frame
+    frame = ttk.Frame(content_frame, padding="10")
+    frame.pack()
+
+    # Cria um objeto que exibe apenas os cabeçalhos das colunas
+    table = ttk.Treeview(frame, columns=("ID", "Nome", "Email", "Contacto", "Morada", "Localidade", "Nascimento","Password", "Perfil"), show="headings")
+
+    # Define as configurações de largura para cada coluna
+    table.column("ID", width=50)
+    table.column("Nome", width=100)
+    table.column("Email", width=150)
+    table.column("Contacto", width=80)
+    table.column("Morada", width=100)
+    table.column("Localidade", width=100)
+    table.column("Nascimento", width=70)
+    table.column("Password", width=70)
+    table.column("Perfil", width=50)
+
+    # Define os cabeçalhos das colunas
+    table.heading("ID", text="ID")
+    table.heading("Nome", text="Nome")
+    table.heading("Email", text="Email")
+    table.heading("Contacto", text="Contacto")
+    table.heading("Morada", text="Morada")
+    table.heading("Localidade", text="Localidade")
+    table.heading("Nascimento", text="Nascimento")
+    table.heading("Password", text="Password")
+    table.heading("Perfil", text="Perfil")
+
+    # Cria uma barra de rolagem vertical e associa-a à visualização vertical da tabela.
+    scroll = ttk.Scrollbar(frame, orient="vertical", command=table.yview)
+    table.configure(yscrollcommand=scroll.set)
+
+    # Cria 3 botões associados
+    btn_atualizar = ttk.Button(content_frame, text="Atualizar", command=alterar_utilizador)
+    btn_apagar = ttk.Button(content_frame, text="Apagar", command=apagar_utilizador)
+    btn_criar = ttk.Button(content_frame, text="Criar", command=criar_utilizador)
+
+    # Empacota os botões
+    btn_atualizar.pack(pady=5)
+    btn_apagar.pack(pady=5)
+    btn_criar.pack(pady=5)
+
+    # Empacota a tabela à esquerda e a barra de rolagem à direita
+    table.pack(side="left")
+    scroll.pack(side="right", fill="y")
+
+    # Exibe os registros na tabela
+    buscar_registros()
---
+++ menu_utilizadores.py
@@ -0,0 +1,102 @@
+from tkinter import *
+
+import g_utilizadores
+
+# Definição de funções que limpam o content_frame (chamando a função clear_content_frame())
+# e criam um rótulo com os nomes respetivos. O rótulo é adicionado ao content_frame
+def show_gestao_utilizadores():
+    clear_content_frame()
+    label = Label(content_frame, text='Informações de Gestão de Utilizadores', font=('Arial', 14))
+    label.pack(pady=20)
+
+def show_gestao_alunos():
+    clear_content_frame()
+    label = Label(content_frame, text='Informações de Gestão de Alunos', font=('Arial', 14))
+    label.pack(pady=20)
+
+def show_gestao_aulas_horarios():
+    clear_content_frame()
+    label = Label(content_frame, text='Informações de Gestão de Aulas e Horários', font=('Arial', 14))
+    label.pack(pady=20)
+
+def show_gestao_pagamentos():
+    clear_content_frame()
+    label = Label(content_frame, text='Informações de Gestão de Pagamentos', font=('Arial', 14))
+    label.pack(pady=20)
+
+def show_avaliacao_alunos():
+    clear_content_frame()
+    label = Label(content_frame, text='Informações de Avaliação de Alunos', font=('Arial', 14))
+    label.pack(pady=20)
+
+def show_performance_alunos():
+    clear_content_frame()
+    label = Label(content_frame, text='Informações de Performance de Alunos', font=('Arial', 14))
+    label.pack(pady=20)
+
+def clear_content_frame():
+    for widget in content_frame.winfo_children():
+        widget.destroy()
+
+# Definir a aparência inicial da janela principal
+root = Tk() # Cria uma instância da classe
+root.title('Centro de formação') # Título da janela
+root.geometry('1280x640+280+150') # Geometria da janela, especifica a largura, altura e posição inicial
+root.resizable(0, 0) # Bloquea a capacidade de alterar o tamanho em largura e altura
+
+# Define a fonte das letras exibidas na interface
+FONT = ('Arial', 12)
+
+# Cria um frame dentro da janela raiz, define a sua altura, largura, cor de fundo
+# Empacota o frame no local padrão e expande para ocupar o espaço disponível
+main_frame = Frame(root, width=1280, height=720, bg='#F5F5F5')
+main_frame.pack(fill='both', expand=True)
+
+# Cria um frame dentro do main_frame, define a sua altura, largura, cor de fundo
+# Empacota o frame à esquerda e preenche verticalmente o espaço disponível
+menu_frame = Frame(main_frame, bg='#383838', width=200, height=720)
+menu_frame.pack(side='left', fill='y')
+
+# Cria um frame dentro do content_frame, define a sua altura, largura, cor de fundo
+# Empacota o frame à esquerda e expande para ocupar o espaço disponível
+content_frame = Frame(main_frame, bg='white', width=1080, height=720)
+content_frame.pack(side='left', fill='both', expand=True)
+
+# Define um dictionary para o estilo dos botões
+button_styles = {
+    'bg': '#008080',
+    'fg': 'white',
+    'activebackground': '#4C4C4C',
+    'activeforeground': 'white',
+    'font': FONT,
+    'borderwidth': 0,
+    'highlightthickness': 0,
+    'relief': 'flat',
+    'cursor': 'hand2',
+}
+
+# Define vários botões a partir do dicionário button_styles
+# Empacota-os com preenchimento vertical (pady=10) preenchimento horizontal (padx=20) e preenchimento horizontal completo (fill='x').
+
+# O primeiro botão (button1) em particular Define a função main_users_def.criar_interface(content_frame)
+# como o comando a ser executado quando o botão é clicado.
+button1 = Button(menu_frame, text='Gestão de Utilizadores', **button_styles, command=lambda: g_utilizadores.criar_interface(content_frame))
+button1.pack(pady=10, padx=20, fill='x')
+
+button2 = Button(menu_frame, text='Gestão de Alunos', **button_styles)
+button2.pack(pady=10, padx=20, fill='x')
+
+button3 = Button(menu_frame, text='Gestão de Aulas e Horários', **button_styles)
+button3.pack(pady=10, padx=20, fill='x')
+
+button4 = Button(menu_frame, text='Gestão de Pagamentos', **button_styles)
+button4.pack(pady=10, padx=20, fill='x')
+
+button5 = Button(menu_frame, text='Avaliações de Alunos', **button_styles)
+button5.pack(pady=10, padx=20, fill='x')
+
+button6 = Button(menu_frame, text='Performance de Alunos', **button_styles)
+button6.pack(pady=10, padx=20, fill='x')
+
+# Inicia a loop do programa e exibe a janela
+root.mainloop()
